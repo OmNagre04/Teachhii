c516acace55254d726cd197f0eded313264d2721
OmNagre04 <omnagare04@gmail.com>
2025-07-25 18:34:36 +0530

    Implement Sentry For Erron Handling

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://acb8f38ffc7d0f3c08e94d113780087f@o4509218235940865.ingest.de.sentry.io/4509729275379792",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
